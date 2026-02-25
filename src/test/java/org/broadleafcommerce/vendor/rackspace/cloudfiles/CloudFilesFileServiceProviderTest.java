da43e9f94ce3d20a2df16deff4cb822139bd78e5
StanislavFedorov <42337700+StanislavFedorov@users.noreply.github.com>
2026-02-25 19:52:50 +0200

    Remove commons-collections from the framework in favor of commons-collections4 (#8)

--- src/test/java/org/broadleafcommerce/vendor/rackspace/cloudfiles/CloudFilesFileServiceProviderTest.java
+++ src/test/java/org/broadleafcommerce/vendor/rackspace/cloudfiles/CloudFilesFileServiceProviderTest.java
@@ -23,7 +23,7 @@
  */
 package org.broadleafcommerce.vendor.rackspace.cloudfiles;
 
-import org.apache.commons.collections.CollectionUtils;
+import org.apache.commons.collections4.CollectionUtils;
 import org.apache.commons.io.FilenameUtils;
 import org.apache.commons.io.IOUtils;
 import org.apache.commons.lang.StringUtils;
